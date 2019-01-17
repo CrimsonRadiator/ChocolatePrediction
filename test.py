38473798d5987a5ff0106b4aeee152420243b06f
Tomasz Sałacki <salackitomasz@gmail.com>
2019-01-17 12:38:55 +0100

    Fixed number of data rows

--- test.py
+++ test.py
@@ -1,31 +1,32 @@
 import NeuralNetwork
+
+
 import bikeDatasetLoader
-import numpy
 
 training_data, result_data = bikeDatasetLoader.readBikeDataSet()
 
-network = NeuralNetwork.Network([10, 5, 1])
+network = NeuralNetwork.Network([12, 50, 1])
 
 paired_data = []
 paired_result_data = []
 
 for x_row, y_row in zip(training_data[0], training_data[1]):
-    paired_data.append([x_row.reshape(10, 1), y_row.reshape(1, 1)])
+    paired_data.append([x_row.reshape(12, 1), y_row.reshape(1, 1)])
 
 for x_row, y_row in zip(result_data[0], result_data[1]):
-    paired_result_data.append([x_row.reshape(10, 1), y_row.reshape(1, 1)])
+    paired_result_data.append([x_row.reshape(12, 1), y_row.reshape(1, 1)])
 
-network.SGD(paired_data, 10, 0.5, 5)
+network.SGD(paired_data, 10, 200, 500, paired_result_data)
 
 tmp = 0
 
-for row in paired_data:
+for row in paired_result_data:
     # continue
-    #print(row[0])
-    if ((row[1] - network.feedforward(row[0])) * 977.0) > 0:
+    # print(row[0])
+    if ((row[1] - network.feedforward(row[0]))) > 0:
         tmp += (row[1] - network.feedforward(row[0])) * 977.0
     else:
         tmp -= (row[1] - network.feedforward(row[0])) * 977.0
-    print('result ', network.feedforward(row[0])*977.0, '\t', row[1]*977.0)
+    print('tmp ', tmp, ' result ', network.feedforward(row[0])*977.0, '\t', row[1]*977.0)
 
-print('\nmean: ', tmp/paired_data.__len__())
+print('\nmean: ', tmp/paired_result_data.__len__())
